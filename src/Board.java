fb8a0e186e0c438e7957e366781a89534ac715a1
ChaosFlurry <chaosflurry@gmail.com>
2018-03-23 00:06:02 -0700

    Added checking logic

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,10 @@
+import java.util.stream.IntStream;
+
 public class Board {
     private int[][] tiles;
 
     public Board(int[][] tiles) {
-        this.tiles = new int[8][8];
+        this.tiles = new int[9][9];
         this.tiles = tiles;
     }
 
@@ -21,21 +23,21 @@ public class Board {
     }
 
     public void setRow(int row, int[] elements) {
-        for (int i = 0; i < elements.length; i++) {
+        for (int i = 0; i < 9; i++) {
             tiles[row - 1][i] = elements[i];
         }
     }
 
     public int[] getColumn(int column) {
-        int[] elements = new int[tiles.length];
-        for (int i = 0; i < tiles.length; i++) {
+        int[] elements = new int[9];
+        for (int i = 0; i < 9; i++) {
             elements[i] = tiles[i][column - 1];
         }
         return elements;
     }
 
     public void setColumn(int column, int[] elements) {
-        for (int i = 0; i < elements.length; i++) {
+        for (int i = 0; i < 9; i++) {
             tiles[i][column - 1] = elements[i];
         }
     }
@@ -44,7 +46,131 @@ public class Board {
         return tiles[row - 1][column - 1];
     }
 
-    public void display() {
+    public void setTile(int row, int column, int value) {
+        tiles[row - 1][column - 1] = value;
+    }
 
+    public int[] getRegion(int region) {
+        int[] elements = new int[9];
+        int x = 0;
+        int y = 0;
+        if (region == 1) {
+            x = 1;
+            y = 1;
+        } else if (region == 2) {
+            x = 1;
+            y = 4;
+        } else if (region == 3) {
+            x = 1;
+            y = 7;
+        } else if (region == 4) {
+            x = 4;
+            y = 1;
+        } else if (region == 5) {
+            x = 4;
+            y = 4;
+        } else if (region == 6) {
+            x = 4;
+            y = 7;
+        } else if (region == 7) {
+            x = 7;
+            y = 1;
+        } else if (region == 8) {
+            x = 7;
+            y = 4;
+        } else if (region == 9) {
+            x = 7;
+            y = 7;
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                elements[3 * i + j] = this.getTile(x + i, y + j);
+            }
+        }
+        return elements;
+    }
+
+    public void setRegion(int region, int[] elements) {
+        int x = 0;
+        int y = 0;
+        if (region == 1) {
+            x = 1;
+            y = 1;
+        } else if (region == 2) {
+            x = 1;
+            y = 4;
+        } else if (region == 3) {
+            x = 1;
+            y = 7;
+        } else if (region == 4) {
+            x = 4;
+            y = 1;
+        } else if (region == 5) {
+            x = 4;
+            y = 4;
+        } else if (region == 6) {
+            x = 4;
+            y = 7;
+        } else if (region == 7) {
+            x = 7;
+            y = 1;
+        } else if (region == 8) {
+            x = 7;
+            y = 4;
+        } else if (region == 9) {
+            x = 7;
+            y = 7;
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.setTile(x + i, y + j, elements[3 * i + j]);
+            }
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (j == 0 || j == 3 || j == 6) {
+                    System.out.print("|\t");
+                }
+                System.out.print(this.getTile(i + 1, j + 1) + "\t");
+            }
+            System.out.print("|");
+            System.out.print("\n");
+            if (i == 2 || i == 5) {
+                System.out.println("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-");
+            }
+        }
+    }
+
+    public boolean isValidRow(int row) {
+        int[] elements = this.getRow(row);
+        return IntStream.of(elements).filter(i -> i > 0).count() > IntStream.of(elements).filter(i -> i > 0).distinct().count();
+    }
+
+    public boolean isValidColumn(int column) {
+        int[] elements = this.getColumn(column);
+        return IntStream.of(elements).filter(i -> i > 0).count() > IntStream.of(elements).filter(i -> i > 0).distinct().count();
+    }
+
+    public boolean isValidRegion(int region) {
+        int[] elements = this.getRegion(region);
+        return IntStream.of(elements).filter(i -> i > 0).count() > IntStream.of(elements).filter(i -> i > 0).distinct().count();
+    }
+
+    public boolean isValidBoard() {
+        for (int i = 0; i < 9; i++) {
+            if (!isValidRow(i)) {
+                return false;
+            }
+            if (!isValidColumn(i)) {
+                return false;
+            }
+            if (!isValidRegion(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,34 +1,15 @@
 public class Sudoku {
     public static void main(String[] args) {
-        int[][] tiles = new int[8][8];
-        for (int i = 0; i < 8; i++) {
-            int[] column = new int[8];
-            for (int j = 0; j < 8; j++) {
+        int[][] tiles = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            int[] column = new int[9];
+            for (int j = 0; j < 9; j++) {
                 // actually these are rows
-                column[j] = j;
+                column[j] = j + 1;
             }
             tiles[i] = column;
         }
-        for (int m = 0; m < 8; m++) {
-            for (int n = 0; n < 8; n++) {
-                System.out.print(tiles[m][n]);
-            }
-            System.out.println("");
-        }
-        System.out.println("");
         Board test = new Board(tiles);
-        int[] testRow = test.getRow(2);
-        for (int k : testRow) {
-            System.out.print(k);
-        }
-        System.out.println("");
-        int[] testColumn = test.getColumn(2);
-        for (int k : testRow) {
-            System.out.print(k);
-        }
-        System.out.println("");
-        System.out.println(test.getTile(2, 2));
-        System.out.println(test.getTile(2, 3));
-        System.out.println(test.getTile(3, 2));
+        test.display();
     }
 }
